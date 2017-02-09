db177757207dc4f5d9a4c33d63dcb7b441db3acc
ejk91 <enochkim91@gmail.com>
2017-02-08 19:43:26 -0800

    Completed Function-shared pattern queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -3,24 +3,35 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
   var newInstance = {
     storage: {},
-    totalCount: 0
+    totalCount: 0,
+    firstCount: 1,
+    propertyCount: 0
   };
   _.extend(newInstance, queueMethods);
   return newInstance;
 };
 
 var queueMethods = {
-  enqueue: function() {
+  enqueue: function(value) {
+    this.propertyCount++;
+    this.storage[this.propertyCount] = value;
+    this.totalCount++;
 
   },
 
   dequeue: function() {
+    var dq = this.storage[this.firstCount];
+    delete this.storage[this.firstCount];
+    if (this.totalCount > 0) {
+      this.totalCount--;
+      this.firstCount++;
+    }
 
+    return dq;
   },
 
   size: function() {
-
-    
+    return this.totalCount;
   }
 
 };
