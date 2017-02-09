df648bf5d0481317f59e759df8cae3fdccbeb32a
ejk91 <enochkim91@gmail.com>
2017-02-08 17:40:25 -0800

    Completed Stack - functional shared pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = {
+    storage: {},
+    totalCount: 0
+  };
+  _.extend(newInstance, queueMethods);
+  return newInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function() {
+
+  },
+
+  dequeue: function() {
+
+  },
+
+  size: function() {
+
+    
+  }
+
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = {
+    storage: {},
+    count: 0  
+  };
+  
+  _.extend(newInstance, stackMethods);
+  return newInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  pop: function () {
+    var popped = this.storage[this.count];
+    if (this.count > 0) {
+      this.count--;
+    }
+    return popped;
+  },
+  
+  push: function (value) {
+    this.count++;
+    this.storage[this.count] = value;
+  },
+  
+  size: function () {
+    return this.count;
+  },
+};
 
 
