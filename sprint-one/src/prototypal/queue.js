46d631faa6a67334619a4b15a8b35209fac4bfa0
ejk91 <enochkim91@gmail.com>
2017-02-08 20:08:12 -0800

    Pass tests for prototypal pattern - Queue and Stack

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.totalCount = 0;
+  newQueue.storage = {};
+  newQueue.firstCount = 1;
+  newQueue.propertyCount = 0;
+  return newQueue;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.totalCount++;
+  this.propertyCount++;
+  this.storage[this.propertyCount] = value;
+};
+
+queueMethods.dequeue = function() {
+  var dq = this.storage[this.firstCount];
+  delete this.storage[this.firstCount];
+  if (this.totalCount > 0) {
+    this.totalCount--;
+    this.firstCount++;
+  }
+  return dq;
+};
+
+queueMethods.size = function() {
+  return this.totalCount;
+};
 
 
