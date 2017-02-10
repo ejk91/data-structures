17e6da9dd0341f6632fc4aa5595c0a57d24e5291
ejk91 <enochkim91@gmail.com>
2017-02-09 16:03:54 -0800

    Completes Linked-List tests.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,19 +3,51 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+
+
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+    }
+    list.head.next = newNode;
+    list.tail = newNode;
+
+//{head: {value: 5;, next: tail}, tail: {value: 5, next: null}}
+
   };
 
   list.removeHead = function() {
+    if (!list.head) {
+      return null;
+    }
+    var oldHead = list.head.value;
+    //console.log('old head', this.head);
+    //console.log(this.head.next);
+    this.head = this.head.next;
+    //console.log('new head', this.head);
+    return oldHead;
   };
 
   list.contains = function(target) {
+   //value = target
+   //if not current's next
+   //start at head node
+    var currentNode = list.head;
+    while (currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return false;
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function(value) { //creates the node itself with info
   var node = {};
 
   node.value = value;
